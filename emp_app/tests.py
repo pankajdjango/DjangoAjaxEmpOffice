d2941dbb26a16e063fc737b03f257f7c41a9eae5
pankajdjango <pankajdjango@gmail.com>
2022-03-23 00:41:46 +0530

    l

--- emp_app/tests.py
+++ emp_app/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+# import sys,date
+# # Create your tests here.
+# argv = sys.argv
+# print('argv',argv)
+# if argv:
+#     date=argv[0]
+#     print("date",date)
+# else:
+    #  t_date=date.today()
+#     print("t_date",t_date)
+import hashlib
 
-# Create your tests here.
+password = "MD5Online"
+db_password = "d49019c7a78cdaac54250ac56d0eda8a"
+encode = hashlib.md5(password.encode())
+decode = hashlib.md5(password.encode()).hexdigest()
+
+
+
+print("encode= ",encode)
+print("decode= ",decode)
+
+if (hashlib.md5(password.encode()).hexdigest() == db_password):
+   print("Authentication success")
+else:
+   print("Bad login or password")
+
+import requests
+
+#Initialization
+url = "https://www.md5online.org/api.php"
+key = "YOUR_API_KEY"
+md5 = "d3c8e06e57cc1af7ebdba01427e62bc2"
+
+#Request
+result = requests.get(url+"?p="+key+"&h="+md5)
+print(result.response)
